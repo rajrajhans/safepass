06c7ee7fbe200abd506f00c7c3591932f17103a2
Raj Rajhans <raj.rajhans18@vit.edu>
2020-12-06 21:36:34 +0530

    Update Dashboard Display

--- src/components/MyPasswords.jsx
+++ src/components/MyPasswords.jsx
@@ -1,20 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AuthContext } from './helpers/AuthContext';
 import AddNewPassword from './helpers/AddNewPassword';
+import Button from 'react-bootstrap/Button';
+import '../styles/MyPasswords.css';
 
 const MyPasswords = () => {
+  const [isAddPwdActive, setIsAddPwdActive] = useState(false);
   const { currentUser } = useContext(AuthContext);
 
   const handleClose = () => {
-    console.log('Closed');
+    setIsAddPwdActive(false);
   };
 
   return (
     <>
+      <AddNewPassword isActive={isAddPwdActive} handleClose={handleClose} />
       <div>
-        Hello, {currentUser.email}
-        <br />
-        <AddNewPassword isActive={true} handleClose={handleClose} />
+        <div className="welcomeText">Hello, {currentUser.email}</div>
+        <Button onClick={() => setIsAddPwdActive(true)}>
+          Add New Password
+        </Button>
       </div>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css';
+import '../src/styles/main.css';
 import App from './App';
 
 ReactDOM.render(
